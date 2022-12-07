4a655b897a34c985ddae9523a8c765aa5761e9f0
Huy <73681373+dnhuy4869@users.noreply.github.com>
2022-12-07 16:09:23 +0700

    clear cache

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
-header('Cache-Control: no cache'); //no cache
-session_cache_limiter('private_no_expire'); // works
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+
 session_start();
 
 ?>
