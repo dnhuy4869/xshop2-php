d8aa22ca3b4f9bc66f9e0b107aab48c35b843a4b
Huy <73681373+dnhuy4869@users.noreply.github.com>
2022-12-12 16:35:48 +0700

    .

--- views/home.php
+++ views/home.php
@@ -201,28 +201,28 @@
             <div class="col">
                 <div class="owl-carousel vendor-carousel">
                     <div class="vendor-item border p-4">
-                        <img src="../img/vendor-1.jpg" alt="">
+                        <img src="img/vendor-1.jpg" alt="">
                     </div>
                     <div class="vendor-item border p-4">
-                        <img src="../img/vendor-2.jpg" alt="">
+                        <img src="img/vendor-2.jpg" alt="">
                     </div>
                     <div class="vendor-item border p-4">
-                        <img src="../img/vendor-3.jpg" alt="">
+                        <img src="img/vendor-3.jpg" alt="">
                     </div>
                     <div class="vendor-item border p-4">
-                        <img src="../img/vendor-4.jpg" alt="">
+                        <img src="img/vendor-4.jpg" alt="">
                     </div>
                     <div class="vendor-item border p-4">
-                        <img src="../img/vendor-5.jpg" alt="">
+                        <img src="img/vendor-5.jpg" alt="">
                     </div>
                     <div class="vendor-item border p-4">
-                        <img src="../img/vendor-6.jpg" alt="">
+                        <img src="img/vendor-6.jpg" alt="">
                     </div>
                     <div class="vendor-item border p-4">
-                        <img src="../img/vendor-7.jpg" alt="">
+                        <img src="img/vendor-7.jpg" alt="">
                     </div>
                     <div class="vendor-item border p-4">
-                        <img src="../img/vendor-8.jpg" alt="">
+                        <img src="img/vendor-8.jpg" alt="">
                     </div>
                 </div>
             </div>
